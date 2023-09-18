68b85b9694f2aa2ed88724b0640b3bf85824c39c
shadrxcc <akaadesm@gmail.com>
2023-09-18 19:30:45 +0100

    manage form state

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,17 +1,28 @@
+import { useState } from "react";
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div
       id="login"
       className="flex flex-col gap-y-6 justify-center px-4 md:px-20 lg:px-40"
     >
       <h1 className="text-4xl">Sign In</h1>
-      <form action="" className="flex flex-col gap-y-6">
+      <form action="login" onSubmit={handleLogin} className="flex flex-col gap-y-6">
         <span className="flex gap-y-2 flex-col">
           <label htmlFor="email">Email</label>
           <input
             className="bg-transparent px-2 outline-none rounded-lg py-2 border"
             type="email"
             name="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
             id="email"
             placeholder="john@email.com"
           />
@@ -23,6 +34,8 @@ const Login = () => {
             className="bg-transparent px-2 outline-none py-2 rounded-lg border"
             type="password"
             name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             id="password"
           />
         </span>
--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,17 +1,32 @@
+import { useState } from "react";
+
 const Signup = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div
       id="signup"
       className="flex flex-col gap-y-6 justify-center px-4 md:px-20 lg:px-40"
     >
       <h1 className="text-4xl">Sign Up</h1>
-      <form action="" className="flex flex-col gap-y-6">
+      <form
+        action="signup"
+        onSubmit={handleSignup}
+        className="flex flex-col gap-y-6"
+      >
         <span className="flex gap-y-2 flex-col">
           <label htmlFor="email">Email</label>
           <input
             className="bg-transparent px-2 outline-none rounded-lg py-2 border"
             type="email"
             name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             id="email"
             placeholder="john@email.com"
           />
@@ -23,7 +38,9 @@ const Signup = () => {
             className="bg-transparent px-2 outline-none py-2 rounded-lg border"
             type="password"
             name="password"
+            value={password}
             id="password"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </span>
 
